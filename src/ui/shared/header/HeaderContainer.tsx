0846bd998804ae526742fdcb33742b52a33af1f2
kostyabek <kostyabek@gmail.com>
2023-02-15 10:44:21 +0200

    Added missing changes about files moving

--- src/ui/shared/header/HeaderContainer.tsx
+++
@@ -1,5 +0,0 @@
-import { HeaderFragment } from './HeaderFragment';
-
-export const HeaderContainer = (): JSX.Element => {
-  return <HeaderFragment multiplayerLink="/" leaderboardsLink="/" aboutLink="/" />;
-};
