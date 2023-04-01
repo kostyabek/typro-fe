14b09cd98316bc8156f32ddd34f3f2f12653c91f
kostyabek <kostyabek@gmail.com>
2023-04-01 22:35:13 +0300

    Guarded profile page

--- src/ui/groups/profile/pages/ProfilePage/ProfilePage.tsx
+++ src/ui/groups/profile/pages/ProfilePage/ProfilePage.tsx
@@ -1,6 +1,13 @@
 import { Box, Typography } from '@mui/material';
+import { useAppSelector } from '../../../../../state';
+import { Navigate } from 'react-router-dom';
+import { AuthPages, Groups } from '../../../../../utils';
 
 export const ProfilePage = (): JSX.Element => {
+  const isAuthenticated = useAppSelector((store) => store.data.user.isAuthenticated);
+  if (!isAuthenticated) {
+    return <Navigate to={`${Groups.Auth}/${AuthPages.SignIn}`} />;
+  }
   return (
     <Box>
       <Typography>Profile Page</Typography>
