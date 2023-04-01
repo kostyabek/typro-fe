b6369d6be8f84f80bb3d0b1a7ce82149e24d7f8b
kostyabek <kostyabek@gmail.com>
2023-04-01 22:51:56 +0300

    Added is authenticated status setting on app start

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -2,10 +2,17 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { useMemo } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { createMuiTheme } from '../shared';
-import { useAppSelector } from '../state';
+import { useAppDispatch, useAppSelector, userActions } from '../state';
 import { router } from './common';
+import { getAccessToken } from '../utils';
 
 export const App = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const token = getAccessToken();
+  if (token !== null) {
+    dispatch(userActions.setIsAuthenticated(true));
+  }
+
   const themeMode = useAppSelector((store) => store.ui.theme.mode);
   const theme = useMemo(() => createMuiTheme(themeMode), [themeMode]);
 
