d6fcc6d7fd3fc1a7d5666aa9520206907691d8b7
Kostiantyn Biektin <kostyabek@gmail.com>
2024-09-25 18:08:49 +0300

    Migrate to vite. Update packages

---
+++ src/main.tsx
@@ -0,0 +1,34 @@
+import ReactDOM from "react-dom/client";
+import { store } from "./state";
+import { App } from "./ui";
+import { Provider } from "react-redux";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider
+} from "@tanstack/react-query";
+import "./index.css";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  },
+  queryCache: new QueryCache({
+    onError: (error) => {
+      console.error(error);
+    }
+  })
+});
+
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </QueryClientProvider>
+);
